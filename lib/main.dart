0b8441346aa40f9c52638034c4deb21c438cc7fe
emilio kariuki <70817006+emilio-kariuki@users.noreply.github.com>
2022-02-03 21:08:35 +0300

    February 3, 2022 9:08 PM

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,11 @@
 import 'package:custom_app_bar/Screens/home_screen.dart';
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import "generated_plugin_registrant.dart";
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(const MaterialApp(
     debugShowCheckedModeBanner: false,
     home: Home(),
